25dc90d076537524d319d59a398460e51bdda0be
Daniel Botea <danielmbotea@gmail.com>
2021-11-21 22:22:42 +0200

    Add comments

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,4 +1,5 @@
-import React, {useRef, useState, useEffect} from 'react'
+import React, {useRef, useState, useEffect} from 'react';
+import { submitComment } from '../services';
 
 const CommentsForm = ({slug}) => {
   const [error, setError] = useState(false);
@@ -7,11 +8,48 @@ const CommentsForm = ({slug}) => {
   const commentEl = useRef();
   const nameEl = useRef();
   const emailEl = useRef();
-  const toreDataEl = useRef();
+  const storeDataEl = useRef();
+
+  useEffect(()=> {
+    nameEl.current.value = window.localStorage.getItem('name');
+    emailEl.current.value = window.localStorage.getItem('email');
+  }, [])
+
+  const handleCommentSubmission = () => {
+    setError(false);
+    const {value: comment} = commentEl.current;
+    const {value: name} = nameEl.current;
+    const {value: email} = emailEl.current;
+    const {checked: storeData} = storeDataEl.current;
+
+    if(!comment || !name || !email) {
+      setError(true);
+      return;
+    }
+
+    // se foloseste destructuring name: name, email: email
+    const commentObj = {name, email, comment, slug}
+
+    if(storeData) {
+      window.localStorage.setItem('name', name);
+      window.localStorage.setItem('email', email);
+    } else {
+      window.localStorage.removeItem('name', name);
+      window.localStorage.removeItem('email', email);
+    }
+
+    submitComment(commentObj)
+      .then((res) => {
+        setshowSuccessMessage(true)
+        setTimeout(()=> {
+          setshowSuccessMessage(false)
+        }, 3000)
+      })
+  }
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments Form</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
           type="text" 
@@ -21,7 +59,7 @@ const CommentsForm = ({slug}) => {
           name="comment"
         />
       </div>
-      <div className="grid grid-cols-1 gap-4 mb-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
         <input 
           type="text" 
           ref={nameEl}
@@ -29,15 +67,33 @@ const CommentsForm = ({slug}) => {
           placeholder="Name"
           name="name"
         />
+        <input 
+            type="text" 
+            ref={emailEl}
+            className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" 
+            placeholder="Email"
+            name="email"
+          />
       </div>
-      <div className="grid grid-cols-1 gap-4 mb-4">
-      <input 
-          type="text" 
-          ref={emailEl}
-          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" 
-          placeholder="Email"
-          name="email"
-        />
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+        <div>
+          <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" />
+          <label className="text-gray-500 cursor-pointer ml-2" htmlFor="storeData"> Save my e-mail name for the next time I comment.</label>
+        </div>
+      </div>
+      
+      {error && <p className="text-xs text-red-500">All fields are required</p>}
+      <div className="mt-8">
+        <button 
+          type="button" 
+          onClick={handleCommentSubmission}
+          className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
+        >
+          Post Comment
+        </button>
+
+        {showSuccessMessage && <span className="text-xl font-semibold mt-3 text-green-500">Comment submitted for review</span>}
       </div>
     </div>
   )
